4b3e50aef703106c770ba3886c1118416b1ba070
Eissanoor <eissaanoor@gmail.com>
2025-10-26 21:00:31 +0500

    Remove unique constraint from supplier email field in supplier model to allow duplicate email entries, enhancing flexibility in supplier management.

--- models/supplierModel.js
+++ models/supplierModel.js
@@ -10,7 +10,6 @@ const supplierSchema = new mongoose.Schema(
     },
     email: {
       type: String,
-      unique: true,
       trim: true,
       lowercase: true,
       match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please provide a valid email address'],
